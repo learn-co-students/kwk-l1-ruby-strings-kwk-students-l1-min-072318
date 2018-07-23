9f0f95aa547a38fa78c52f1d7eb0b760582a753b
Grace5275 <grace_madson@outlook.com>
2018-07-23 19:41:31 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,25 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+puts "Hi, you've been invited to a party! What is your name?"
+
+guest_name = gets.chomp 
+
+puts "What is your party's name?"
+
+party_name = gets.chomp 
+
+puts "What is the party date?"
+
+date = gets.chomp 
+
+puts "What is the time of the party?"
+
+time = gets.chomp 
+
+puts "Who is the host?"
+
+host_name = gets.chomp 
+
+puts "Dear #{guest_name}, You are cordially invited to #{party_name} on #{date} at #{time}. Sincerely, #{host_name}"
